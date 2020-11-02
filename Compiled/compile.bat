615d4cee5f2d7a9db3b4f162bd4a6bfb2d317dd0
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2020-11-02 01:49:15 -0600

    Office Reactor V1.2
    Textured and Lit

--- Compiled/compile.bat
+++ Compiled/compile.bat
@@ -18,7 +18,7 @@ echo --------------VIS---------------
 C:\Users\Brooklyn\Downloads\quake-leveldesign-starterkit-master\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\vis.exe levelblockout
 
 echo -------------LIGHT--------------
-C:\Users\Brooklyn\Downloads\quake-leveldesign-starterkit-master\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\light.exe levelblockout
+C:\Users\Brooklyn\Downloads\quake-leveldesign-starterkit-master\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\light.exe -soft -extra4 levelblockout
 
 copy levelblockout.bsp C:\Users\Brooklyn\Downloads\quake-leveldesign-starterkit-master\quake-leveldesign-starterkit-master\id1\Maps
 copy levelblockout.pts C:\Users\Brooklyn\Downloads\quake-leveldesign-starterkit-master\quake-leveldesign-starterkit-master\id1\Maps
